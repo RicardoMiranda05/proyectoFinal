ffb195429f543bcaa034e49bc03e5f7adbcb8f17
Brayan Montiel <braymontiel77@gmail.com>
2025-11-19 21:05:55 -0600

    Se completó la clase Usuario y se definieron los métodos obligatorios de Administrador, junto al método para crear un usuario.

--- AppGestion/src/Administrador.java
+++ AppGestion/src/Administrador.java
@@ -8,8 +8,40 @@ import java.util.UUID;
  * el pull request será rechazado.
  * ----- Mensaje genérico -----
  * 
+ * ----- Mensaje para VISITANTES -----
+ * El método crearUsuario todavía no está disponible para su uso, ya que las clases
+ * Desarrollador e Invitado no han definido sus métodos constructores en este punto del
+ * proyecto.
+ * ----- Mensaje para VISITANTES -----
+ * 
  * INSERTA TU PROPIO COMENTARIO DE LA CLASE.
  */
 public class Administrador extends Usuario {
-    
+    /**
+     * 
+     * @param nombre Nombre del usuario.
+     * @param nickname Nickname del usuario.
+     * @param email Correo del usuario.
+     * @param password Contraseñña del usuario.
+     * @param rol Rol que el usuario creado desempeñará (administrador, desarrollador o invitado).
+     * @return
+     */
+    public Usuario crearUsuario (String nombre, String nickname, String email, String password, String rol) {
+        switch (rol) {
+            case "Administrador":
+                return new Administrador(nombre, nickname, email, password);
+            case "Desarrollador":
+                return new Desarrollador(nombre, nickname, email, password);
+            case "Invitado":
+                return new Invitado(nombre, nickname, email, password);
+            default:
+                System.out.println("No existe el rol " + rol + ".");
+                return null;
+        }
+    }
+    /* ----- CONSTRUCTORES ----- */
+    public Administrador (String nombre, String nickname, String email, String password) {
+        super(nombre, nickname, email, password);
+    }
+    /* ----- CONSTRUCTORES ----- */
 }
--- AppGestion/src/Usuario.java
+++ AppGestion/src/Usuario.java
@@ -10,6 +10,82 @@ import java.util.UUID;
  * 
  * INSERTA TU PROPIO COMENTARIO DE LA CLASE.
  */
-public abstract class Usuario {
-    
+public class Usuario {
+    private final String id;
+    private String nombre;
+    private String nickname;
+    private String email;
+    private String password;
+
+
+    public void verTareas() {
+
+    }
+    /* ----- CONSTRUCTOR ----- */
+    protected Usuario (String nombre, String nickname, String email, String password) {
+        super();
+        id = UUID.randomUUID().toString();
+        this.nombre = nombre;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
+    /* ----- CONSTRUCTOR ----- */
+
+    /* ----- ACCESO ----- */
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        System.out.println("El ID no puede ser sobreescrito.");
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    /* ----- ACCESO ----- */
+    /* ----- UTILERÍA ----- */
+    @Override
+    public String toString() {
+        String salida = getClass().toString().substring(6);
+        salida += " [id=" + id + 
+                    ", nombre=" + nombre +
+                    ", nickname=" + nickname +
+                    ", email=" + email +
+                    ", password=" + password + "]";
+        return salida;
+    }
+    /**
+     * Compara dos usuarios mediante su ID.
+     * @param user Usuario con el cual this se va a comparar.
+     * @return true si this tiene el mismo ID que usuario y false si no.
+     */
+    public boolean equals(Usuario user) {
+        if (this == user)
+            return true;
+        if (user == null)
+            return false;
+        return id == user.id;
+    }
+    /* ----- UTILERÍA ----- */
 }
